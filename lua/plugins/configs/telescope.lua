72428732f85464a0b5152f3e8b377a09e16ed9ac
Ryan Keswick <ryank415@gmail.com>
2025-03-26 09:44:44 +1100

    Initial commit of my Neovim configuration

---
+++ lua/plugins/configs/telescope.lua
@@ -0,0 +1,11 @@
+return {
+  defaults = {
+    prompt_prefix = "   ",
+    selection_caret = " ",
+    entry_prefix = " ",
+    sorting_strategy = "ascending",
+    layout_config = {
+      horizontal = { prompt_position = "top" },
+    },
+  },
+}
