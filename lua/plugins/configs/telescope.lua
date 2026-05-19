168c62082e3e7458049346fc3e4d51555235e6f8
Ryan Keswick <ryan.keswick@canva.com>
2026-05-19 02:52:21 +0000

    Add braille spinner to telescope status text

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -1,11 +1,51 @@
 local threads = vim.trim(vim.fn.system("nproc"))
 
+local spinner_frames = { "⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷" }
+local spinner_idx = 0
+
+local function get_status_text(self, opts)
+  local multi_select_cnt = #(self:get_multi_selection())
+  local showing_cnt = (self.stats.processed or 0) - (self.stats.filtered or 0)
+  local total_cnt = self.stats.processed or 0
+
+  local status_icon
+  if opts and not opts.completed then
+    spinner_idx = (spinner_idx + 1) % #spinner_frames
+    status_icon = spinner_frames[spinner_idx + 1]
+  else
+    status_icon = ""
+  end
+
+  local status_text
+  if showing_cnt == 0 and total_cnt == 0 then
+    status_text = status_icon
+  elseif multi_select_cnt == 0 then
+    status_text = string.format("%s %s / %s", status_icon, showing_cnt, total_cnt)
+  else
+    status_text = string.format("%s %s / %s / %s", status_icon, multi_select_cnt, showing_cnt, total_cnt)
+  end
+
+  -- workaround for extmark right_align side-scrolling limitation
+  -- https://github.com/nvim-telescope/telescope.nvim/issues/2929
+  local prompt_width = vim.api.nvim_win_get_width(self.prompt_win)
+  local cursor_col = vim.api.nvim_win_get_cursor(self.prompt_win)[2]
+  local strings = require("plenary.strings")
+  local prefix_display_width = strings.strdisplaywidth(self.prompt_prefix)
+  local prefix_width = #self.prompt_prefix
+  local prefix_shift = prefix_display_width ~= prefix_width and prefix_display_width or 0
+  if (prompt_width - cursor_col - #status_text + prefix_shift) < 0 then
+    return ""
+  end
+  return status_text
+end
+
 return {
   defaults = {
     prompt_prefix = "   ",
     selection_caret = " ",
     entry_prefix = " ",
     sorting_strategy = "ascending",
+    get_status_text = get_status_text,
     layout_config = {
       horizontal = { prompt_position = "top" },
     },
